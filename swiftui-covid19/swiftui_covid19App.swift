8e970657c4489eb59d122c69e8c267a4b003c959
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-01 14:31:42 -0300

    chore: default structure

---
+++ swiftui-covid19/swiftui_covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_covid19App.swift
+//  swiftui-covid19
+//
+//  Created by WAGNER GOULART on 01/05/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

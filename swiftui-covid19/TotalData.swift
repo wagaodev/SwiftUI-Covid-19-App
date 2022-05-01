b585c871b120fd4b7cf70289304b94fdb6916c97
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-01 18:17:30 -0300

    feat: model totalDate created

--- swiftui-covid19/TotalData.swift
+++ swiftui-covid19/TotalData.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+struct TotalDataObject: Codable {
+  let data: TotalData
+}
+
 struct TotalData: Codable {
-  
+  let confirmed: Int
+  let deaths: Int
+  let active: Int
+  let fatality_rate: Double
 }
